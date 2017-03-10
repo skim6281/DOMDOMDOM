f1539f0e82464b53b54c61df6b89f4b04c72aafd
skim6281 <skim6281@gmail.com>
2017-03-10 13:47:09 -0500

    Add demo

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: './lib/main',
+  output: {
+    path: './lib',
+    filename: 'domdomdom.js'
+  },
+  resolve: {
+    extensions: ['.js', '*']
+  },
+  devtool: 'source-maps'
+};
